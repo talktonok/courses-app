02279ceaaa056f11c43651c93605f24763a5b4d7
Ibrahim Nok Mansur <mansuribrahimnok@gmail.com>
2023-04-05 10:25:14 +0100

    Setup server

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { initialize} from './middleware/auth.js';
+import messageRouter from "./routers/MessageRouter.js";
+import { studentRouter } from './routers/StudentsRoutes.js';
+import { userRouter } from './routers/UserRouter.js';
+const app = express();
+dotenv.config();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(initialize());
+app.use('/api/message', messageRouter);
+app.use('/api/students', studentRouter);
+app.use('/api/user', userRouter);
+
+
+app.get('/', (req, res) => {
+    res.send({
+        message: 'Welcome to this API.',
+    })
+})
+
+app.get("/ping", (req, res) => {
+    res.status(200);
+    return res.json("Pong")
+});
+
+export default app;
